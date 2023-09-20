df5f950ed1f74556120c0c58b80de1ca7d5b4c2d
DELL <DELL@DESKTOP-D17R2SR>
2023-09-20 12:53:02 +0530

    My First Commit

---
+++ ReportProject/src/main/java/Judo/HubReport.java
@@ -0,0 +1,24 @@
+package Judo;
+
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class HubReport {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		ExtentReports  report=new ExtentReports();
+		ExtentSparkReporter spark=new ExtentSparkReporter(".\\report\\reports.html");
+		report.attachReporter(spark);
+		
+		
+		report.flush();
+		Desktop.getDesktop().browse(new File("C:\\JAVA Program\\ReportProject\\report\\reports.html").toURI());
+
+	}
+
+}
